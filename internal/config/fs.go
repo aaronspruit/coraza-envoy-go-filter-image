54cc70184b94ff38631fd685ee5c9e250ee2a46a
aaronspruit <spruit@aaronspruit.com>
2026-04-01 12:59:15 -0700

    fix formatting

--- internal/config/fs.go
+++ internal/config/fs.go
@@ -29,7 +29,7 @@ func init() {
 			"@crs-setup-conf":      "crs-setup.conf.example",
 		},
 		map[string]string{
-			"@owasp_crs": "crs",
+			"@owasp_crs":     "crs",
 			"@owasp_plugins": "plugins",
 		},
 	}
